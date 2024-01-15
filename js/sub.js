3ee485b2b60c1083832ebd2ed687e0fe06a5ea68
KANGSUM1N <155428211+KANGSUM1N@users.noreply.github.com>
2024-01-15 14:55:19 +0900

    Add files via upload

--- js/sub.js
+++ js/sub.js
@@ -1,11 +1,9 @@
 $(document).ready(function () {
 
-
     $(".tab_content").hide();
     $("ul.tabs li:first").addClass("active").show();
     $(".tab_content:first").show();
     $("ul.tabs li").click(function () {
-
         $("ul.tabs li").removeClass("active");
         $(this).addClass("active");
         $(".tab_content").hide();
@@ -16,17 +14,21 @@ $(document).ready(function () {
 
     //다른 페이지에서 이동할때 필요한 코드
     var location = window.location.hash;
-    var tab_arr = [];
-    $(".tabs li span").each(function (index, element) {
-        tab_arr.push($(this).attr("data"));
-    });
+    if ($(location).length != "0") {
+        var tab_arr = [];
+        $(".tabs li span").each(function (index, element) {
+            tab_arr.push($(this).attr("data"));
+        });
 
-    var numb = jQuery.inArray(location, tab_arr)
-    $('.tabs li').removeClass("active");
-    $('.tabs li').eq(numb).addClass("active");
-    $(".tab_content1").hide();
-    $('.tab_container1').find(location).show();
+        var numb = jQuery.inArray(location, tab_arr)
+        console.log(numb);
+        $('.tabs li').removeClass("active");
+        $('.tabs li').eq(numb).addClass("active");
+        $(".tab_content1").hide();
+        $('.tab_container1').find(location).show();
 
-    return false;
+        return false;
+    }
+
 
 });
